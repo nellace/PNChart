c845af7a13d79d4a11603585ee41ac8072152b09
nellace <nellace@qq.com>
2015-08-10 11:09:15 +0800

    Update PNColor.m
    Random color for pieChart

--- PNChart/PNColor.m
+++ PNChart/PNColor.m
@@ -25,5 +25,17 @@
     return img;
 }
 
++ (UIColor *)randomColor
+{
+    static BOOL seeded = NO;
+    if (!seeded) {
+        seeded = YES;
+        srandom(time(NULL));
+    }
+    CGFloat red =  (CGFloat)random()/(CGFloat)RAND_MAX;
+    CGFloat blue = (CGFloat)random()/(CGFloat)RAND_MAX;
+    CGFloat green = (CGFloat)random()/(CGFloat)RAND_MAX;
+    return [UIColor colorWithRed:red green:green blue:blue alpha:1.0f];
+}
 
 @end
